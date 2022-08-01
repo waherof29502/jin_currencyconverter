771a2272710db49f2fc63b67b459ff66351e2c34
JinPing <39645652+waherof29502@users.noreply.github.com>
2022-08-01 13:21:38 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import express from 'express';
+
+const app = express();
+const getCountries = async (currencyCode) => {
+  try {
+    const response = await axios.get(
+      `https://restcountries.com/v3.1/currency/${currencyCode}`
+    );
+
+    return response.data.map((country) => country.name.common);
+  } catch (error) {
+    throw new Error(`Unable to get countries that use ${currencyCode}`);
+  }
+};
+
+const convertCurrency = async (fromCurrency, toCurrency, amount) => {
+  const countries = await getCountries(toCurrency);
+  const convertedAmount = (amount * Math.random() * 11).toFixed(2);
+
+  return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
+};
+
+app.get('/', async (req, res) => {
+  const { fromCurrency, toCurrency, amount } = req.query;
+  const message = await convertCurrency(fromCurrency, toCurrency, amount);
+
+  res.json(message);
+});
+
+app.listen(6000, () => {
+  console.log('Server is running on port 6000');
+});
+
+// convertCurrency('USD', 'CHF', 20)
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
